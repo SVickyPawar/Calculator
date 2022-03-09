6b91ed11ca710c40f8e871f1743db91bc99a9c5f
SVickyPawar <97332040+SVickyPawar@users.noreply.github.com>
2022-03-10 01:08:57 +0530

    Assignment
    calculator assignment initiated

---
+++ main.js
@@ -0,0 +1,13 @@
+const add=require('./add');
+const subtract=require('./subtract');
+const multiply=require('./multiply');
+const devide=require('./devide');
+
+console.log(add(5,3));
+
+console.log(devide(10,2));
+
+console.log(subtract(10,2));
+
+console.log(multiply(10,2));
+
